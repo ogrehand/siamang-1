0da43c5336d518afdc79d99d926e3ded72196e0c
azzamsa <oktinastuzi@gmail.com>
2017-05-27 14:20:05 +0700

    adding first product

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,25 +1,22 @@
 <!doctype html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-    <!-- for bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ URL::to('src/css/app.css') }}">
 
     @yield('styles')
-  </head>
-  <body>
-    @include('partials.header')
-    <div class="container">
-      @yield('content')
-    </div>
+</head>
+<body>
+@include('partials.header')
+<div class="container">
+    @yield('content')
+</div>
 
-    <script   src="https://code.jquery.com/jquery-1.12.3.min.js"   integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ="   crossorigin="anonymous"></script>
-    <!-- for bootstrap script-->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-    @yield('scripts')
-  </body>
-</html>
-
+<script   src="https://code.jquery.com/jquery-1.12.3.min.js"   integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ="   crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+@yield('scripts')
+</body>
+</htm
--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -1,11 +1,25 @@
 @extends('layouts.master')
 
 @section('title')
-    Siamang Shop
+  Siamang Shop
 @endsection
 
 @section('content')
-    <h1>it works</h1>
+  <div class="row">
+    <div class="col-sm-6 col-md-4">
+      <div class="thumbnail">
+        <img src="http://wpc.21684.lambdacdn.net/8021684/cdn/media/catalog/product/cache/1/image/c96a280f94e22e3ee3823dd0a1a87606/2/0/2043581561.jpg" alt="...">
+        <div class="caption">
+          <h3>Judul Buku</h3>
+          <p class="description">Demikian pula, tidak adakah orang yang mencintai atau mengejar atau ingin mengalami penderitaan, bukan semata-mata karena penderitaan itu sendiri, tetapi karena sesekali terjadi keadaan di mana susah-payah dan penderitaan dapat memberikan kepadanya kesenangan yang besar.</p>
+          <p>
+            <div class="clearfix">
+              <div class="pull-left price">Rp 1.000</div>
+              <a href="#" class="btn btn-primary pull-right" role="button">Beli</a></p>
+            </div>
+         
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
-
-    
